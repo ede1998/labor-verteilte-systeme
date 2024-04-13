e48689ebb552059788cbbc2a71ec144af3d08aac
ede1998 <online@erik-hennig.me>
2024-04-13 11:04:10 +0200

    implementation exercise 1

--- client_server/src/bin/client.rs
+++ client_server/src/bin/client.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+//! Temperature controller client
+//! Connects with REQ socket to tcp://*:5556
+
+use std::time::Duration;
+
+use anyhow::Context;
+use bytes::Bytes;
+use client_server::protobuf::{Measurement, Request};
+use prost::Message;
+
+fn main() -> anyhow::Result<()> {
+    let context = zmq::Context::new();
+    let client = context.socket(zmq::REQ)?;
+
+    client
+        .connect("tcp://localhost:5556")
+        .context("Failed to connect socket")?;
+
+    loop {
+        let request = Request {};
+        let buffer = request.encode_to_vec();
+        client.send(buffer, 0)?;
+
+        let bytes: Bytes = client.recv_bytes(0)?.into();
+        let measurement = Measurement::decode(bytes).context("Failed to decode message")?;
+        println!("Received {measurement:?}");
+
+        std::thread::sleep(Duration::from_millis(1000));
+    }
 }
--- client_server/src/bin/server.rs
+++ client_server/src/bin/server.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+//! Temperature sensor server
+//! Binds REP socket to tcp://*:5556
+
+use anyhow::Context;
+use bytes::Bytes;
+use client_server::protobuf::{Measurement, Request, TemperatureUnit};
+use prost::Message;
+use rand::{rngs::ThreadRng, Rng};
+
+fn main() -> anyhow::Result<()> {
+    let context = zmq::Context::new();
+    let server = context.socket(zmq::REP)?;
+
+    server.bind("tcp://*:5556")?;
+
+    let mut rng = rand::thread_rng();
+
+    loop {
+        let bytes: Bytes = server.recv_bytes(0)?.into();
+        let request = Request::decode(bytes).context("Failed to decode message")?;
+        println!("Received {request:?}");
+        let measurement = random_measurement(&mut rng);
+        let buffer = measurement.encode_to_vec();
+        server.send(buffer, 0)?;
+    }
+}
+
+fn random_measurement(rng: &mut ThreadRng) -> Measurement {
+    let unit: TemperatureUnit = rng.gen();
+    let value = rng.gen_range(-100.0..1000.0);
+
+    let mut message = Measurement {
+        value,
+        ..Default::default()
+    };
+    message.set_unit(unit);
+    message
 }
