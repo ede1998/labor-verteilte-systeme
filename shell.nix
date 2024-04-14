7c873b5c760375053dd8708c9960dfb183503c78
ede1998 <online@erik-hennig.me>
2024-04-14 20:57:55 +0200

    create nix.shell for zipkin

--- shell.nix
+++ shell.nix
@@ -1,4 +1,54 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+  lib ? pkgs.lib,
+  stdenv ? pkgs.stdenv,
+  fetchurl ? pkgs.fetchurl,
+  makeWrapper ? pkgs.makeWrapper,
+  jre ? pkgs.jre,
+}:
+let
+  rust-overlay = (
+    import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz")
+  );
+  pkgs = (import <nixpkgs> { overlays = [ rust-overlay ]; });
+  zipkin = stdenv.mkDerivation rec {
+    version = "3.2.1";
+    pname = "zipkin-server";
+    src = fetchurl {
+      url = "https://search.maven.org/remote_content?g=io.zipkin&a=zipkin-server&v=${version}&c=exec";
+      sha256 = "GZll+hknrWpBN35WXNUwvdUaw53fA/cRtgYoaBdXwbQ=";
+    };
+    nativeBuildInputs = [ makeWrapper ];
+
+    buildCommand = ''
+      mkdir -p $out/share/java
+      cp ${src} $out/share/java/zipkin-server-${version}-exec.jar
+      mkdir -p $out/bin
+      makeWrapper ${jre}/bin/java $out/bin/zipkin-server \
+        --add-flags "-jar $out/share/java/zipkin-server-${version}-exec.jar"
+    '';
+    meta = with lib; {
+      description = "Zipkin distributed tracing system";
+      homepage = "https://zipkin.io/";
+      sourceProvenance = with sourceTypes; [ binaryBytecode ];
+      license = licenses.asl20;
+      platforms = platforms.unix;
+      maintainers = [ maintainers.hectorj ];
+      mainProgram = "zipkin-server";
+    };
+  };
+in
 pkgs.mkShell {
-    packages = [ pkgs.protobuf ];
+
+  packages = [
+    pkgs.nixfmt-rfc-style
+    pkgs.protobuf
+    (pkgs.rust-bin.beta.latest.default.override {
+      extensions = [
+        "rust-src"
+        "rust-analyzer"
+      ];
+    })
+    zipkin
+  ];
 }
